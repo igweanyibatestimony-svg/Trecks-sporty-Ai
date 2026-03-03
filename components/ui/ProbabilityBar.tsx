5eeb81a6284d0113179135112c43ddcf9880642a
Trecks <igweanyibatestimony@gmail.com>
2026-03-03 01:47:33 +0100

    Update ProbabilityBar.tsx

--- components/ui/ProbabilityBar.tsx
+++ components/ui/ProbabilityBar.tsx
@@ -1,1 +1,9 @@
-
+export default function ProbabilityBar({ home, draw, away }: { home: number; draw: number; away: number }) {
+  return (
+    <div className="w-full h-2 flex rounded-full overflow-hidden">
+      <div style={{ width: `${home}%` }} className="bg-blue-600" />
+      <div style={{ width: `${draw}%` }} className="bg-gray-500" />
+      <div style={{ width: `${away}%` }} className="bg-red-600" />
+    </div>
+  );
+}
