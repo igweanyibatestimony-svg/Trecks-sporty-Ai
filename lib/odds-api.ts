619c4216a461a0ca7866e90227e6043168b6c65e
Trecks <igweanyibatestimony@gmail.com>
2026-03-03 01:40:28 +0100

    Update odds-api.ts

--- lib/odds-api.ts
+++ lib/odds-api.ts
@@ -1,1 +1,6 @@
-
+export async function fetchUpcomingOdds(sport = 'soccer') {
+  const res = await fetch(
+    `https://api.the-odds-api.com/v4/sports/${sport}/odds?apiKey=${process.env.ODDS_API_KEY}&regions=uk&markets=h2h&oddsFormat=decimal`
+  );
+  return res.json();
+}
