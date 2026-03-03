43f271366052914f8a4da9d68b5c834ca5ce5955
Trecks <igweanyibatestimony@gmail.com>
2026-03-03 01:46:38 +0100

    Update Footer.tsx

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,1 +1,7 @@
-
+export default function Footer() {
+  return (
+    <footer className="bg-black border-t border-gray-800 p-4 text-center text-gray-500">
+      © 2025 OracleX. Not for real betting.
+    </footer>
+  );
+}
