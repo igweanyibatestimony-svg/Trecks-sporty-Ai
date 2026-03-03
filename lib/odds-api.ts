867c6e1450dcdc541e2e995221c7377e8f492d4b
Trecks <igweanyibatestimony@gmail.com>
2026-03-03 16:31:15 +0100

    Delete lib directory

--- lib/odds-api.ts
+++
@@ -1,6 +0,0 @@
-export async function fetchUpcomingOdds(sport = 'soccer') {
-  const res = await fetch(
-    `https://api.the-odds-api.com/v4/sports/${sport}/odds?apiKey=${process.env.ODDS_API_KEY}&regions=uk&markets=h2h&oddsFormat=decimal`
-  );
-  return res.json();
-}
